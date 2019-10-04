da7a26da7ff3036fb4208d396ab5fec2bdcc2578
HcwXd <e04dl47878@gmail.com>
2019-10-04 12:34:12 +0800

    Setup _document.js for global css

--- pages/_document.js
+++ pages/_document.js
@@ -18,7 +18,57 @@ export default class MyDocument extends Document {
           {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
 
           {/* Global css settings */}
-          <style jsx global>{``}</style>
+          <style jsx global>{`
+            body,
+            div,
+            ul,
+            ol,
+            li,
+            h1,
+            h2,
+            h3,
+            h4,
+            h5,
+            h6,
+            pre,
+            form,
+            fieldset,
+            input,
+            textarea,
+            p,
+            blockquote,
+            th,
+            td {
+              margin: 0;
+              padding: 0;
+            }
+            * {
+              -webkit-box-sizing: border-box;
+              -moz-box-sizing: border-box;
+              box-sizing: border-box;
+              -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+              font-weight: normal;
+              -webkit-font-smoothing: antialiased;
+              -moz-osx-font-smoothing: grayscale;
+              -webkit-text-size-adjust: 100%;
+            }
+            ul,
+            ol,
+            li {
+              margin: 0;
+              padding: 0;
+              list-style: none;
+            }
+            input:focus,
+            button:focus,
+            textarea:focus {
+              outline: none;
+            }
+            html,
+            body {
+              height: 100%;
+            }
+          `}</style>
         </Head>
 
         <body className="custom_class">
