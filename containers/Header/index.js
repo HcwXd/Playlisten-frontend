76e79bbfe013af78d8152879ec7aa85ea0fcdf86
HcwXd <e04dl47878@gmail.com>
2019-10-08 19:14:08 +0800

    Finish basic header styling

--- containers/Header/index.js
+++ containers/Header/index.js
@@ -15,17 +15,27 @@ class Header extends Component {
     return (
       <div
         id="top-header"
-        className="top-0 left-0 right-0 z-20 flex items-center justify-between border-b">
-        <div>Playlisten</div>
-        <nav className="flex items-center mr-0">
-          <div data-name="/signup" className="ml-8">
-            <Link href="/signup" prefetch>
-              <a>signup</a>
+        className="top-0 left-0 right-0 z-20 flex items-center justify-between fixed shadow py-2 px-4 bg-white">
+        <div className="p-2">Playlisten</div>
+        <nav className="flex items-center">
+          <div data-name="/profile" className="p-2 ml-8">
+            <Link href="/profile" prefetch>
+              <a>Profile</a>
             </Link>
           </div>
-          <div data-name="/login" className="ml-8">
+          <div data-name="/publish" className="p-2 ml-8">
+            <Link href="/publish" prefetch>
+              <a>Publish</a>
+            </Link>
+          </div>
+          <div data-name="/signup" className="p-2 ml-8">
+            <Link href="/signup" prefetch>
+              <a>Signup</a>
+            </Link>
+          </div>
+          <div data-name="/login" className="p-2 ml-8">
             <Link href="/login" prefetch>
-              <a>login</a>
+              <a>Login</a>
             </Link>
           </div>
         </nav>
