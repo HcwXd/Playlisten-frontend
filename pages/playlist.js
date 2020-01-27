ec566b5e779072313f63695a5c42f0bc02baa3f5
HcwXd <e04dl47878@gmail.com>
2020-01-27 17:31:03 +0800

    fix: move SSR Header to pages level

--- pages/playlist.js
+++ pages/playlist.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Query, Mutation, ApolloConsumer } from 'react-apollo';
 import { gql } from 'apollo-boost';
+import Head from 'next/head';
 import Layout from '../components/Layout';
 import Playlist from '../containers/Playlist';
 
@@ -20,6 +21,29 @@ const PlaylistPage = ({ metaData }) => (
   <ApolloConsumer>
     {client => (
       <Layout>
+        <Head>
+          <title>
+            {metaData
+              ? `${metaData.playlistName} by ${metaData.ownerName}`
+              : 'Playlisten'}
+          </title>
+          <meta
+            property="og:title"
+            content={
+              metaData
+                ? `${metaData.playlistName} by ${metaData.ownerName}`
+                : 'Playlisten'
+            }
+          />
+          <meta
+            property="og:description"
+            content={
+              metaData
+                ? `${metaData.playlistDes}`
+                : 'Playlisten - Share and Discover Music Playlist'
+            }
+          />
+        </Head>
         <Playlist client={client} metaData={metaData} />
       </Layout>
     )}
