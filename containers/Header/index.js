ac9ce4965412745175a000828203191979e3d8f0
HcwXd <e04dl47878@gmail.com>
2020-01-31 18:39:23 +0800

    refactor: Split Header render function into smaller sub function

--- containers/Header/index.js
+++ containers/Header/index.js
@@ -12,14 +12,11 @@ class Header extends Component {
   constructor(props) {
     super(props);
     this.state = { showSignup: false, showSignin: false };
-    this.toggleMessageBox = this.toggleMessageBox.bind(this);
     this.toggleSignupForm = this.toggleSignupForm.bind(this);
     this.toggleSigninForm = this.toggleSigninForm.bind(this);
     this.handleSignout = this.handleSignout.bind(this);
-  }
-
-  toggleMessageBox() {
-    this.setState({ openMessage: !this.state.openMessage });
+    this.renderLeftHeader = this.renderLeftHeader.bind(this);
+    this.renderRightHeader = this.renderRightHeader.bind(this);
   }
 
   handleSignout() {
@@ -37,6 +34,65 @@ class Header extends Component {
     this.setState({ showSignin: !this.state.showSignin });
   }
 
+  renderLeftHeader() {
+    return (
+      <nav className="flex items-center">
+        <a href="/">
+          <img className="h-6 mr-4" src={Logo} />
+        </a>
+        <div data-name="/explore" className="p-2 ml-8">
+          <Link href="/explore" prefetch>
+            <a>Explore</a>
+          </Link>
+        </div>
+      </nav>
+    );
+  }
+
+  renderRightHeader() {
+    const isMember = process.browser && localStorage.getItem('userId');
+    return (
+      <nav className="flex items-center">
+        {isMember ? (
+          <React.Fragment>
+            <div data-name="/profile" className="p-2 ml-8">
+              <div className="flex items-center justify-between">
+                <a
+                  href={`/profile?userId=${localStorage.getItem('userId')}`}
+                  className="cursor-pointer">
+                  Profile
+                </a>
+              </div>
+            </div>
+            <div data-name="/publish" className="p-2 ml-8">
+              <Link href="/publish" prefetch>
+                <a>Publish</a>
+              </Link>
+            </div>
+            <div data-name="/signout" className="p-2 ml-8">
+              <a className="cursor-pointer" onClick={this.handleSignout}>
+                Sign out
+              </a>
+            </div>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <div data-name="/signup" className="p-2 ml-8">
+              <a className="cursor-pointer" onClick={this.toggleSignupForm}>
+                Sign up
+              </a>
+            </div>
+            <div data-name="/signin" className="p-2 ml-8">
+              <a className="cursor-pointer" onClick={this.toggleSigninForm}>
+                Sign in
+              </a>
+            </div>
+          </React.Fragment>
+        )}
+      </nav>
+    );
+  }
+
   render() {
     return (
       <React.Fragment>
@@ -49,59 +105,8 @@ class Header extends Component {
         <div
           id="top-header"
           className="top-0 left-0 right-0 z-20 flex items-center justify-between fixed shadow py-2 px-4 bg-white">
-          <nav className="flex items-center">
-            <a href="/">
-              <img className="h-6 mr-4" src={Logo} />
-            </a>
-            <div data-name="/explore" className="p-2 ml-8">
-              <Link href="/explore" prefetch>
-                <a>Explore</a>
-              </Link>
-            </div>
-          </nav>
-
-          <nav className="flex items-center">
-            {process.browser && localStorage.getItem('userId') ? (
-              <React.Fragment>
-                <div data-name="/profile" className="p-2 ml-8">
-                  <div>
-                    <div className="flex items-center justify-between">
-                      <a
-                        href={`/profile?userId=${localStorage.getItem(
-                          'userId',
-                        )}`}
-                        className="cursor-pointer">
-                        Profile
-                      </a>
-                    </div>
-                  </div>
-                </div>
-                <div data-name="/publish" className="p-2 ml-8">
-                  <Link href="/publish" prefetch>
-                    <a>Publish</a>
-                  </Link>
-                </div>
-                <div data-name="/signout" className="p-2 ml-8">
-                  <a className="cursor-pointer" onClick={this.handleSignout}>
-                    Sign out
-                  </a>
-                </div>
-              </React.Fragment>
-            ) : (
-              <React.Fragment>
-                <div data-name="/signup" className="p-2 ml-8">
-                  <a className="cursor-pointer" onClick={this.toggleSignupForm}>
-                    Sign up
-                  </a>
-                </div>
-                <div data-name="/signin" className="p-2 ml-8">
-                  <a className="cursor-pointer" onClick={this.toggleSigninForm}>
-                    Sign in
-                  </a>
-                </div>
-              </React.Fragment>
-            )}
-          </nav>
+          {this.renderLeftHeader()}
+          {this.renderRightHeader()}
         </div>
       </React.Fragment>
     );
