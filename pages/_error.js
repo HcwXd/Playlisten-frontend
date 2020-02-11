9711abe61dd536d8321cf653728be30bdc85722d
HcwXd <e04dl47878@gmail.com>
2020-02-12 06:05:21 +0800

    feat: Add better error page

--- pages/_error.js
+++ pages/_error.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
-const Error = () => <div>Error</div>;
+const Error = ({ statusCode }) => (
+  <div className="w-screen h-screen flex justify-around items-center text-4xl">
+    {statusCode === 404 ? (
+      <div>
+        <div>{`There's NOTHING here ...`}</div>
+        <div className="text-xl">{`... maybe the page you're looking for is gone or never existed`}</div>
+      </div>
+    ) : (
+      `Sorry! Here's a ${statusCode} error 😅`
+    )}
+  </div>
+);
 
 // For i18n usage, import the locales language file you need for this page
-Error.getInitialProps = async () => ({
-  namespacesRequired: [],
-});
+Error.getInitialProps = async ({ res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode, namespacesRequired: [] };
+};
 
 export default Error;
